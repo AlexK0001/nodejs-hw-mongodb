ec7f8572538b388d3d4cc844b33b8f1fe50433f2
Alexandr <alexandr.kopyletz@gmail.com>
2024-10-23 22:11:47 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const PORT = Number(env('PORT', '3001'));
 
-export const setupServer = () => {
+export const startServer = () => {
   const app = express();
 
   app.use(express.json());
