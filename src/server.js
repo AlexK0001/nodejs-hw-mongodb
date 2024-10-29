d05f11c3cce9cb3ee88b35ba1fcc6fbe5e2c7a3f
Alexandr <alexandr.kopyletz@gmail.com>
2024-10-29 14:53:51 +0200

    29.10

--- src/server.js
+++ src/server.js
@@ -3,7 +3,8 @@ import pino from 'pino-http';
 import cors from 'cors';
 import dotenv from "dotenv";
 import { env } from './utils/env.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+
+import contactsRouter from './routers/contacts.js'
 
 dotenv.config();
 
@@ -29,35 +30,7 @@ export const startServer = () => {
     });
   });
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-
-    res.status(200).json({
-      status: 200,
-      message: "Successfully found contacts!",
-      data: contacts,
-    });
-  });
-
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-
-    // Відповідь, якщо контакт не знайдено
-	if (!contact) {
-	  res.status(404).json({
-		  message: 'Contact not found'
-	  });
-	  return;
-	}
-
-	// Відповідь, якщо контакт знайдено
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
+  app.use(contactsRouter);
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
