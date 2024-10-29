8600f0bb2551c39cc22355bd09017b4c0033a86a
Alexandr <alexandr.kopyletz@gmail.com>
2024-10-29 22:35:08 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -15,7 +15,9 @@ const PORT = Number(env('PORT', '3001'));
 export const startServer = () => {
   const app = express();
 
-  app.use(express.json());
+  app.use(express.json({
+    type: ['application/json', 'application/vnd.api+json'],
+  }));
   app.use(cors());
 
   app.use(
