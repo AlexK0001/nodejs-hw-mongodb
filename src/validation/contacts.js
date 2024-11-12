e5272b1a46a6f5add6ade567b229ffbc54c890fd
Alexandr <alexandr.kopyletz@gmail.com>
2024-11-12 18:06:30 +0200

    Update contacts.js

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
-  phoneNumber: Joi.string().integer().min(6).max(15).required(),
+  phoneNumber: Joi.string().pattern(/^\d+$/).min(6).max(15).required(),
   email: Joi.string().email().min(3).max(20),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal').required(),
@@ -10,7 +10,7 @@ export const createContactSchema = Joi.object({
 
 export const updateContactSchema = Joi.object({
     name: Joi.string().min(3).max(20),
-    phoneNumber: Joi.string().integer().min(6).max(15),
+    phoneNumber: Joi.string().pattern(/^\d+$/).min(6).max(15),
     email: Joi.string().email().min(3).max(20),
     isFavourite: Joi.boolean(),
     contactType: Joi.string().valid('work', 'home', 'personal'),
