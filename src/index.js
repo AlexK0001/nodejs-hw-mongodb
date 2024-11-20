82353dd58b4eee9c982ef0fca7d576b3e250b9bc
Alexandr <alexandr.kopyletz@gmail.com>
2024-11-20 15:45:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 
 import { startServer } from './server.js';
-import { initMongoDB } from './db/initMongoDB.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
 import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
 import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
 
 const bootstrap = async () => {
-  await initMongoDB();
+  await initMongoConnection();
   await createDirIfNotExists(TEMP_UPLOAD_DIR);
   await createDirIfNotExists(UPLOAD_DIR);
   startServer();
