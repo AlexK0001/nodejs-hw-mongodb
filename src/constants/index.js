320a677995af60c67b518224be184adfe6c9a78f
Alexandr <alexandr.kopyletz@gmail.com>
2024-11-20 16:20:23 +0200

    20.11/1

--- src/constants/index.js
+++ src/constants/index.js
@@ -22,7 +22,7 @@ export const SORT_ORDER = {
   export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
 
   export const CLOUDINARY = {
-    CLOUD_NAME: 'dlvth1zpm',
-    API_KEY: '713318978773945',
-    API_SECRET: 'gU69S23HJczjKPGKsGfgWRjkJwU',
+    CLOUD_NAME: 'CLOUD_NAME',
+    API_KEY: 'API_KEY',
+    API_SECRET: 'API_SECRET',
   };
