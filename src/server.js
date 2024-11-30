8448fd6c7e4d9f1b01272e06c12c85d36168774a
Alexandr <alexandr.kopyletz@gmail.com>
2024-11-30 14:56:47 +0200

    30.11

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 dotenv.config();
 
@@ -31,6 +32,8 @@ export const startServer = () => {
   );
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+
+  app.use('/api-docs', swaggerDocs());
 
   app.get('/', (req, res) => {
     res.json({
